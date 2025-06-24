63aaa767ffe205c422fbedee2966c792b7c3dd91
Vu Quang Hoa Le <145735861+Acatwalker88@users.noreply.github.com>
2025-06-24 00:10:45 -0400

    Started new react project

---
+++ src/nav/Nav.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+
+export default class Nav extends Component {
+  render() {
+    return <div>This is the nav page</div>;
+  }
+};
